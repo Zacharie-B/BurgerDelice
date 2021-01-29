6bfef1c995f91d152b24844da0a93dc67a8dcc5d
Zacharie <zbaril@orange.fr>
2021-01-29 15:04:06 +0100

    bonnes classes de données

--- BurgerDelice/src/data/Stock.java
+++ BurgerDelice/src/data/Stock.java
@@ -3,21 +3,42 @@ package data;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Stock{
-	private Map<Ingredient, Integer> ingredients = new HashMap<Ingredient, Integer>();
-
-	public Stock(Map<Ingredient, Integer> ingredients) {
-		super();
-		this.ingredients = ingredients;
+public class Stock {
+	
+	private Map <Ingredient, Integer> stock = new HashMap <Ingredient, Integer>();
+	
+	public Stock (HashMap <Ingredient, Integer> stock) {
+		this.stock = stock;
 	}
 
-	public Map<Ingredient, Integer> getIngredients() {
-		return ingredients;
+	public Map<Ingredient, Integer> getStock() {
+		return stock;
 	}
 
-	public void setIngredients(Map<Ingredient, Integer> ingredients) {
-		this.ingredients = ingredients;
+	public void setStock(Map<Ingredient, Integer> stock) {
+		this.stock = stock;
 	}
 	
-	pub
+	public void add(Ingredient ingredient, int quantite) {
+		if(!stock.containsKey(ingredient)) {
+			stock.put(ingredient, quantite);
+		}
+		else {
+			stock.put(ingredient, stock.get(ingredient) + quantite);
+		}
+	}
+	
+	public void remove(Ingredient ingredient, int quantite) throws IllegalArgumentException {
+		if(stock.get(ingredient) - quantite < 0) {
+			throw new IllegalArgumentException("Invalide parameter");
+		}
+		
+		if(stock.containsKey(ingredient)) {
+			stock.put(ingredient, stock.get(ingredient) - quantite);
+		}
+		else {
+			throw new IllegalArgumentException("Invalide parameter");
+		}
+	}
+
 }

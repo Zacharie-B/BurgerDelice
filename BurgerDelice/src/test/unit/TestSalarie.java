55e605e35f5442ec0e5829d0dbeaa29565699e5f
Thibaut <Thibaut@DESKTOP-88TK90P>
2021-04-09 11:35:27 +0200

    test salary

--- BurgerDelice/src/test/unit/TestSalarie.java
+++ BurgerDelice/src/test/unit/TestSalarie.java
@@ -1,5 +1,61 @@
 package test.unit;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import config.GameConfiguration;
+import data.Block;
+import data.Customer;
+import data.RestaurantMap;
+import process.CustomerManager;
+import process.PayOfEmployee;
+import process.Payment;
+import process.RestaurantBuilder;
+import process.RestaurantManager;
+
 public class TestSalarie {
 
+	private RestaurantManager restaurantManager;
+	private RestaurantBuilder restaurantBuilder;
+	private RestaurantMap restaurantMap;
+	private CustomerManager customerManager;
+	private Customer customer;
+	private Payment payOfEmployee;
+	
+	@Before
+	public void prepareTest() {
+		Block block = new Block(GameConfiguration.LINE_ORDER, GameConfiguration.COLUMN_ENTRY);
+		this.customer = new Customer(block, 1, 1, false, true, 2);
+		this.restaurantManager = new RestaurantManager();
+		this.restaurantBuilder = new RestaurantBuilder();
+		this.restaurantMap = restaurantBuilder.buildMap();
+		this.restaurantManager = this.restaurantBuilder.buildElements(this.restaurantMap);
+		this.customerManager = new CustomerManager(this.restaurantManager);
+		this.restaurantManager.addCustomer(this.customer);
+		this.restaurantManager.addTakenBlock(block);
+		
+		this.payOfEmployee = new PayOfEmployee(restaurantManager, 1);
+	}
+	
+	@Test
+	public void testPayment() {
+		//System.out.println("Money = " + this.restaurantManager.getMoney());
+		assertEquals(0, (long) this.restaurantManager.getMoney());
+		for(int time=0 ; time<5 ; time++) {
+			customerManager.moveCustomer();
+		}
+		//System.out.println("Money = " + this.restaurantManager.getMoney());
+		assertNotEquals(0, this.restaurantManager.getMoney());
+	}
+	
+	@Test
+	public void PayOfEmployee() {
+		this.restaurantManager.addMoney(100000);
+		assertEquals(100000, (long) this.restaurantManager.getMoney());
+		this.payOfEmployee.managePayment();
+		assertNotEquals(100000, this.restaurantManager.getMoney());
+	}
 }
